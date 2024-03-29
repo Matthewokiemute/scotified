473b0acf7c7cc09c298bba882c6a5e01b034dadc
Matthewokiemute <ego.okiemute@gmail.com>
2024-03-29 09:24:54 +0100

    removed register section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import Features from "@/components/Features";
 import GetApp from "@/components/GetApp";
 import Hero from "@/components/Hero";
-import RegisterSection from "@/components/RegisterSection";
+// import RegisterSection from "@/components/RegisterSection";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Features />
-      <RegisterSection />
+      {/* <RegisterSection /> */}
       <GetApp />
     </>
   );
